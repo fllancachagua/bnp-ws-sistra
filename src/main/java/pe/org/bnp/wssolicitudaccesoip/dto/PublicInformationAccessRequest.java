3c01ff9b1cdb65297c3a76d507cfd4f4f3b888a1
FelipeLlC <desarrollo03.otie@bnp.gob.pe>
2018-06-18 11:05:14 -0500

    Versión 1.1.1, se modificó el metodo getListAccessRequestPI para incluir nuevos campos de bd SISTRA

--- src/main/java/pe/org/bnp/wssolicitudaccesoip/dto/PublicInformationAccessRequest.java
+++ src/main/java/pe/org/bnp/wssolicitudaccesoip/dto/PublicInformationAccessRequest.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 @XmlRootElement
-@XmlType(propOrder = { "id", "documentNumber","requestDate","documentName","user","issue","remark","originTypeName","procedureName","folios","originOfficeName","destinationOfficeName","claimant","classification","documentTypeName","destinationTypeName","fileName","filePath","deadLine","state"})
+@XmlType(propOrder = { "id", "documentNumber","requestDate","documentName","user","issue","remark","originTypeName","originTypeCode","procedureName","procedureCode","folios","originOfficeName","originOfficeCode","destinationOfficeName","destinationOfficeCode","claimant","classificationName","classificationCode","documentTypeName","documentTypeCode","destinationTypeName","destinationTypeCode","fileName","filePath","deadLine","stateName","stateCode"})
 public class PublicInformationAccessRequest {
     private String id;
     private String documentNumber;
@@ -18,14 +18,23 @@ public class PublicInformationAccessRequest {
     private String remark;
     private String originOfficeName;
     private String claimant;
-    private String classification;
+    private String classificationName;
     private String documentTypeName;
     private String destinationTypeName;
     private String fileName;
     private String filePath;
     private String deadLine;
-    private String state;
+    private String stateName;
     private String destinationOfficeName;
+    
+    private String classificationCode;
+    private String destinationOfficeCode;
+    private String destinationTypeCode;
+    private String documentTypeCode;
+    private String originOfficeCode;
+    private String originTypeCode;
+    private String procedureCode;
+    private String stateCode;
 
     public String getId() {
         return id;
@@ -75,6 +84,22 @@ public class PublicInformationAccessRequest {
         this.issue = issue;
     }
 
+    public String getOriginTypeName() {
+        return originTypeName;
+    }
+
+    public void setOriginTypeName(String originTypeName) {
+        this.originTypeName = originTypeName;
+    }
+
+    public String getProcedureName() {
+        return procedureName;
+    }
+
+    public void setProcedureName(String procedureName) {
+        this.procedureName = procedureName;
+    }
+
     public String getFolios() {
         return folios;
     }
@@ -107,36 +132,12 @@ public class PublicInformationAccessRequest {
         this.claimant = claimant;
     }
 
-    public String getClassification() {
-        return classification;
+    public String getClassificationName() {
+        return classificationName;
     }
 
-    public void setClassification(String classification) {
-        this.classification = classification;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getOriginTypeName() {
-        return originTypeName;
-    }
-
-    public void setOriginTypeName(String originTypeName) {
-        this.originTypeName = originTypeName;
-    }
-
-    public String getProcedureName() {
-        return procedureName;
-    }
-
-    public void setProcedureName(String procedureName) {
-        this.procedureName = procedureName;
+    public void setClassificationName(String classificationName) {
+        this.classificationName = classificationName;
     }
 
     public String getDocumentTypeName() {
@@ -147,12 +148,20 @@ public class PublicInformationAccessRequest {
         this.documentTypeName = documentTypeName;
     }
 
-    public String getDeadLine() {
-        return deadLine;
+    public String getDestinationTypeName() {
+        return destinationTypeName;
     }
 
-    public void setDeadLine(String deadLine) {
-        this.deadLine = deadLine;
+    public void setDestinationTypeName(String destinationTypeName) {
+        this.destinationTypeName = destinationTypeName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 
     public String getFilePath() {
@@ -163,20 +172,20 @@ public class PublicInformationAccessRequest {
         this.filePath = filePath;
     }
 
-    public String getDestinationTypeName() {
-        return destinationTypeName;
+    public String getDeadLine() {
+        return deadLine;
     }
 
-    public void setDestinationTypeName(String destinationTypeName) {
-        this.destinationTypeName = destinationTypeName;
+    public void setDeadLine(String deadLine) {
+        this.deadLine = deadLine;
     }
 
-    public String getState() {
-        return state;
+    public String getStateName() {
+        return stateName;
     }
 
-    public void setState(String state) {
-        this.state = state;
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
     }
 
     public String getDestinationOfficeName() {
@@ -187,8 +196,75 @@ public class PublicInformationAccessRequest {
         this.destinationOfficeName = destinationOfficeName;
     }
 
+    public String getClassificationCode() {
+        return classificationCode;
+    }
+
+    public void setClassificationCode(String classificationCode) {
+        this.classificationCode = classificationCode;
+    }
+
+    public String getDestinationOfficeCode() {
+        return destinationOfficeCode;
+    }
+
+    public void setDestinationOfficeCode(String destinationOfficeCode) {
+        this.destinationOfficeCode = destinationOfficeCode;
+    }
+
+    public String getDestinationTypeCode() {
+        return destinationTypeCode;
+    }
+
+    public void setDestinationTypeCode(String destinationTypeCode) {
+        this.destinationTypeCode = destinationTypeCode;
+    }
+
+    public String getDocumentTypeCode() {
+        return documentTypeCode;
+    }
+
+    public void setDocumentTypeCode(String documentTypeCode) {
+        this.documentTypeCode = documentTypeCode;
+    }
+
+    public String getOriginOfficeCode() {
+        return originOfficeCode;
+    }
+
+    public void setOriginOfficeCode(String originOfficeCode) {
+        this.originOfficeCode = originOfficeCode;
+    }
+
+    public String getOriginTypeCode() {
+        return originTypeCode;
+    }
+
+    public void setOriginTypeCode(String originTypeCode) {
+        this.originTypeCode = originTypeCode;
+    }
+
+    public String getProcedureCode() {
+        return procedureCode;
+    }
+
+    public void setProcedureCode(String procedureCode) {
+        this.procedureCode = procedureCode;
+    }
+
+    public String getStateCode() {
+        return stateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        this.stateCode = stateCode;
+    }
+
     @Override
     public String toString() {
-        return "PublicInformationAccessRequest{" + "id=" + id + ", documentNumber=" + documentNumber + ", requestDate=" + requestDate + ", documentName=" + documentName + ", user=" + user + ", issue=" + issue + ", originTypeName=" + originTypeName + ", procedureName=" + procedureName + ", folios=" + folios + ", remark=" + remark + ", originOfficeName=" + originOfficeName + ", claimant=" + claimant + ", classification=" + classification + ", documentTypeName=" + documentTypeName + ", destinationTypeName=" + destinationTypeName + ", fileName=" + fileName + ", filePath=" + filePath + ", deadLine=" + deadLine + ", state=" + state + ", destinationOfficeName=" + destinationOfficeName + '}';
+        return "PublicInformationAccessRequest{" + "id=" + id + ", documentNumber=" + documentNumber + ", requestDate=" + requestDate + ", documentName=" + documentName + ", user=" + user + ", issue=" + issue + ", originTypeName=" + originTypeName + ", procedureName=" + procedureName + ", folios=" + folios + ", remark=" + remark + ", originOfficeName=" + originOfficeName + ", claimant=" + claimant + ", classificationName=" + classificationName + ", documentTypeName=" + documentTypeName + ", destinationTypeName=" + destinationTypeName + ", fileName=" + fileName + ", filePath=" + filePath + ", deadLine=" + deadLine + ", stateName=" + stateName + ", destinationOfficeName=" + destinationOfficeName + ", classificationCode=" + classificationCode + ", destinationOfficeCode=" + destinationOfficeCode + ", destinationTypeCode=" + destinationTypeCode + ", documentTypeCode=" + documentTypeCode + ", originOfficeCode=" + originOfficeCode + ", originTypeCode=" + originTypeCode + ", procedureCode=" + procedureCode + ", stateCode=" + stateCode + '}';
     }
+
+
+
 }
